8dc3a128bd0617ac878deef001b97fa4e5858c3f
Chintan Vachhani <chintan.vachhani10@gmail.com>
2018-02-16 03:50:40 -0800

    aws deployment changes.

--- mysql.js
+++ mysql.js
@@ -1,22 +1,27 @@
 var Sequelize = require('sequelize');
 
-// Database Setup
+// Database Setup for localhost
 var sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
+    host: 'localhost',
+    dialect: 'mysql',
 
-  pool: {
-    max: 250,
-    min: 0,
-    idle: 10000,
-  },
+    pool: {
+        max: 250,
+        min: 0,
+        idle: 10000,
+    },
 });
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+
+// Database Setup for aws elastic beanstalk
+/*var sequelize = new Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+    host: process.env.RDS_HOSTNAME,
+    dialect: 'mysql',
+
+    pool: {
+        max: 250,
+        min: 0,
+        idle: 10000,
+    },
+});*/
 
 module.exports = sequelize;
